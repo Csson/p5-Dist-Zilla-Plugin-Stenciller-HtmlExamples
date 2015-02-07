9518d7a7e10565dbb45f5179532978cae73340e5
Erik Carlsson <info@code301.com>
2015-02-07 22:22:15 +0100

    Init

---
+++ lib/Dist/Zilla/Plugin/Stenciller/HtmlExamples.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::Stenciller::HtmlExamples;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::Stenciller::HtmlExamples;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::Stenciller::HtmlExamples is ...
+
+=head1 SEE ALSO
+
+=cut
